bfd8c3689953234f7ecc1b2014326c7ffcbf812b
melvinlim87 <melvinlim87@hotmail.com>
2025-05-25 15:28:53 +0800

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: { 
     domains: ['images.pexels.com', 'via.placeholder.com'],
     unoptimized: true 
-  },
-  experimental: {
-    appDir: true
   },
   webpack: (config, { isServer }) => {
     // Disable webpack cache to prevent EIO errors
