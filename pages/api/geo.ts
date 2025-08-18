02b05dcd4b09b81a0195df0ab39b93966e283eb3
= <=>
2025-08-18 12:26:15 +0800

    update

--- pages/api/geo.ts
+++ pages/api/geo.ts
@@ -7,28 +7,22 @@ import requestIp from 'request-ip';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let ip = requestIp.getClientIp(req);
 
-    if (!ip || ip === '::1' || ip === '127.0.0.1') {
-        ip = '8.8.8.8'; // Google DNS as example
+    console.log('check req header', req.headers);
+    if (!ip || ip === '::1' || ip === '127.0.0.1' || ip.startsWith('::ffff:127.0.0.1')) {
+        ip = (req.headers['x-forwarded-for'] as string)?.split(',')[0]?.trim() || null;
     }
+
+    console.log('inside geo api get ip', ip);
+    // Fallback if still no IP (use Google DNS for dev)
+    if (!ip) ip = '8.8.8.8';
 
     const geo = geoip.lookup(ip);
 
-    console.log('inside geo api', ip);
-    console.log('inside geo api', geo);
+    console.log('inside geo api get geo', geo);
+
 
     return res.status(200).json({
         ip,
         country: geo?.country ?? null,
     });
 }
-
-// export async function GET(req: Request) {
-//   const ip = requestIp.getClientIp({ headers: Object.fromEntries(req.headers) }) || '127.0.0.1';
-//   console.log('inside geo api', ip)
-//   const geo = geoip.lookup(ip);
-//   console.log('inside geo api', geo)
-//   return NextResponse.json({
-//     ip,
-//     country: geo?.country ?? null,
-//   });
-// }
